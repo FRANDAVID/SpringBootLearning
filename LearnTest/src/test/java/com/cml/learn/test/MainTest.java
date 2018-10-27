2bd68c0dc8e603e17c1be1eeb7f68a6eab572f90
David_jin <gdweijin2008@163.com>
2018-10-27 18:42:31 +0800

    Revert "单元测试添加"
    This reverts commit 5da5760ca9a357ffa64cdfd92bad14f260c685e4.

--- LearnTest/src/test/java/com/cml/learn/test/MainTest.java
+++ LearnTest/src/test/java/com/cml/learn/test/MainTest.java
@@ -3,15 +3,12 @@ package com.cml.learn.test;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Main.class)
 public class MainTest {
-    @Autowired
-    private AService aService;
 
     @Before
     public void before() {
@@ -20,7 +17,6 @@ public class MainTest {
 
     @Test
     public void testStartUp() {
-        aService.doSmthing();
         System.out.println("--------------测试----------");
     }
 }
